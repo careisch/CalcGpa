7e3344b00a2d69d4e27df6771ce7479eb215a6d0
careisch <careisch@mtu.edu>
2014-12-15 19:28:10 -0500

    add a drop down menu for grade

--- gpa.js
+++ gpa.js
@@ -1,7 +1,19 @@
 
 
 function gpaCalc(){
-             		var class1Ne = document.getElementById('class1Name').value;
-             		alert('class1Name');
+             		var class1Name = document.forms[0].class1Name.value;
+             		var class2Name = document.forms[0].class2Name.value;
+             		var class3Name = document.forms[0].class3Name.value;
+             		var class4Name = document.forms[0].class4Name.value;
+             		var class5Name = document.forms[0].class5Name.value;
+             		var class6Name = document.forms[0].class6Name.value;
+             		var class7Name = document.forms[0].class7Name.value;
+
+             		var class1Grade = document.forms[0].class1Grade.value;
+
+
+
+
+             		alert(class1Grade);
             	}
        
